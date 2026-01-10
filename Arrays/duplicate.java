54b4e34b8160f0a7623de57da1ccfda6a7514513
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-10 17:00:23 +0530

    My approach but O(N2)

---
+++ Arrays/duplicate.java
@@ -0,0 +1,18 @@
+public class duplicate{
+    public static int find(int arr[]){
+        int duplicate1 = -1;
+        for(int i=0 ; i<arr.length ; i++){
+            for(int j=i+1 ; j<arr.length ; j++){
+                if(arr[i]==arr[j]){
+                    duplicate1 = arr[i];
+                }
+            }
+        }
+        return duplicate1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {3, 1, 3, 4, 2};
+        int duplicate1 = find(arr);
+        System.out.println(duplicate1);
+    }
+}
