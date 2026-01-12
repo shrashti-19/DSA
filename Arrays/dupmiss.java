2bf30cd04a2a3f307bb1301b91aa210a97184477
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-12 15:53:35 +0530

    2d array given if

--- Arrays/dupmiss.java
+++ Arrays/dupmiss.java
@@ -1,32 +1,57 @@
 
-import java.util.ArrayList;
 
 public class dupmiss{
-    public static int[] find(int nums[]){
+    public static int[] find(int grid[][]){
         
-        boolean[] bool = new boolean[nums.length+1];
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int i : nums){
-            if(bool[i]){
-                result.add(i);
+        // boolean[] bool = new boolean[nums.length+1];
+        // ArrayList<Integer> result = new ArrayList<>();
+        // for(int i : nums){
+        //     if(bool[i]){
+        //         result.add(i);
+        //     }
+        //     bool[i] = true;
+        // }
+
+        // for(int i=1 ; i <bool.length ;i++){
+        //     if(!bool[i]){
+        //         result.add(i);
+        //     }
+            
+        // }
+        // int[] ans = new int[result.size()];
+        // for(int i=0 ; i<result.size() ; i++){
+        //     ans[i] = result.get(i);
+        // }
+        // return ans;
+        int  n = grid.length;
+        int repeated = -1, missing = -1;
+
+        boolean[] bool = new boolean[n*n+1];
+
+        for(int[] row : grid){
+            for(int i : row){
+                if(bool[i]){
+                    repeated = i;
+                }
+                bool[i] =true;
             }
-            bool[i] = true;
         }
 
-        for(int i=1 ; i <bool.length ;i++){
+        for(int i=1 ; i<=n*n ; i++){
             if(!bool[i]){
-                result.add(i);
+                missing = i;
+                break;
             }
-            
         }
-        int[] ans = new int[result.size()];
-        for(int i=0 ; i<result.size() ; i++){
-            ans[i] = result.get(i);
-        }
-        return ans;
+
+        return new int[]{repeated, missing};
     }
     public static void main(String args[]){
-        int[] nums = {3,5,4,1,1};
+        int[][] nums = {
+           {1, 2},
+           {3, 3}
+         };
+
         int[] array = find(nums);
 
         for (int x : array) {
