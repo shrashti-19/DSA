8b3d397a204644c7bb79f3b0089326a258d399e8
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-15 18:00:18 +0530

    better approach as thinking like array is of 1d array and modifying binary search for 2d array search.

--- Arrays/search2D.java
+++ Arrays/search2D.java
@@ -1,17 +1,33 @@
 public class search2D{
     public static boolean search(int matrix[][], int target){
-        for(int row[] : matrix){
-            for(int col : row){
-                if(col == target){
-                    return true;
-                }
-            }
-        }
+        // for(int row[] : matrix){
+        //     for(int col : row){
+        //         if(col == target){
+        //             return true;
+        //         }
+        //     }
+        // }
 
+        // return false;
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int low = 0;
+        int high = rows*cols-1;
+
+        while(low<=high){
+            int mid = low +  (high-low)/2;
+            int r = mid/cols;
+            int c = mid%cols;
+
+            if(matrix[r][c] == target) return true;
+            else if(matrix[r][c]<target) low = mid+1;
+            else high = mid-1;
+        }
         return false;
     }
     public static void main(String args[]){
         int matrix[][] = {{1,2,3,4},{5,6,7,8}, {9,10,11,12}};
-        System.out.println(search(matrix,0));
+        System.out.println(search(matrix,4));
     }
 }
