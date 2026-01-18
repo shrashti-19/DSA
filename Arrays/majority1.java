397402674f038987921d765e8aa092b794690362
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-18 19:05:39 +0530

    Boyr moore's algor

--- Arrays/majority1.java
+++ Arrays/majority1.java
@@ -1,19 +1,46 @@
 public class majority1{
     public static int maj(int nums[]){
+        // int n = nums.length;
+        // int[] freq = new int[n+1];
+
+        // for(int num : nums){
+        //     freq[num]++;
+        // }
+
+        // int majority = 0;
+        // for(int i=0 ; i<n; i++){
+        //     if(freq[i]>n/2){
+        //         majority = i;
+        //     }
+        // }
+        // return majority;
         int n = nums.length;
-        int[] freq = new int[n+1];
+        int cnt = 0;
+        int el = 0;
 
-        for(int num : nums){
-            freq[num]++;
-        }
-
-        int majority = 0;
-        for(int i=0 ; i<n; i++){
-            if(freq[i]>n/2){
-                majority = i;
+        for(int i=0 ; i<n ; i++){
+            if(cnt==0){
+                cnt++;
+                el = nums[i];
+            }else if(el == nums[i]){
+                cnt++;
+            }else{
+                cnt--;
             }
         }
-        return majority;
+
+        //verifying the potential candidate
+        int cnt1=0;
+        for(int i=0 ; i<n ; i++){
+            if(nums[i] == el){
+                cnt1++;
+            }
+        }
+
+        if(cnt1>n/2){
+            return el;
+        }
+        return -1;
     }
     public static void main(String args[]){
         int nums[] = {3,2,3};
