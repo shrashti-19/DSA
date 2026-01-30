94688edf35bf987ee4bbc1dc7bfe5f85e0f3eb7f
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-30 17:30:02 +0530

    improved

--- Arrays/longest.java
+++ Arrays/longest.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class longest{
     public static int longsub(int nums[]){
-        int n = nums.length;
+        //int n = nums.length;
         Arrays.sort(nums);
 
         // int length = 1;
@@ -14,17 +14,33 @@ public class longest{
         //     }
         // }
         // return length;
-        int length=1;
-        int maxLen = Integer.MIN_VALUE;
-        for(int i=1 ; i<n ; i++){
-            if(nums[i]-nums[i-1]==1){
-                length++;
-                maxLen = Math.max(maxLen, length);
-            }else{
+        // int length=1;
+        // int maxLen = Integer.MIN_VALUE;
+        // for(int i=1 ; i<n ; i++){
+        //     if(nums[i]-nums[i-1]==1){
+        //         length++;
+        //         maxLen = Math.max(maxLen, length);
+        //     }else{
+        //         length=1;
+        //     }
+        // }
+        // return maxLen;
+
+        int length = 1, maxlen = 1;
+        int n = nums.length;
+
+        for(int i=1 ; i<n; i++){
+            //edge case
+            if(nums.length==0) return 0;
+            //duplicates
+            if(nums[i]==nums[i-1]) continue;
+            if(nums[i]-nums[i-1]==1)length++;
+            else{
                 length=1;
             }
+            maxlen = Math.max(maxlen,length);
         }
-        return maxLen;
+        return maxlen;
     }
     public static void main(String args[]){
         int nums[] = {0,3,7,2,5,8,4,6,0,1};
