c75f5eaedba7649278060cd4b60dde35ee32b810
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-30 17:28:09 +0530

    improved

--- Arrays/longest.java
+++ Arrays/longest.java
@@ -5,18 +5,29 @@ public class longest{
         int n = nums.length;
         Arrays.sort(nums);
 
-        int length = 1;
-        for(int i=0 ; i<n ; i++){
-            for(int j=i+1 ; j<n ; j++){
-                if(nums[j]-nums[i]==1){
-                    length++;
-                }
+        // int length = 1;
+        // for(int i=0 ; i<n ; i++){
+        //     for(int j=i+1 ; j<n ; j++){
+        //         if(nums[j]-nums[i]==1){
+        //             length++;
+        //         }
+        //     }
+        // }
+        // return length;
+        int length=1;
+        int maxLen = Integer.MIN_VALUE;
+        for(int i=1 ; i<n ; i++){
+            if(nums[i]-nums[i-1]==1){
+                length++;
+                maxLen = Math.max(maxLen, length);
+            }else{
+                length=1;
             }
         }
-        return length;
+        return maxLen;
     }
     public static void main(String args[]){
-        int nums[] = {100,4,200,1,3,2};
+        int nums[] = {0,3,7,2,5,8,4,6,0,1};
         System.out.println("Length of longest consecutive subsequence is : " + longsub(nums));
     }
 }
