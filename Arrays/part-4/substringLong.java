8435cb6cdd503fbb1e1a6b76e5b108588424e584
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-02-05 23:29:57 +0530

    sliding window concept used

--- Arrays/part-4/substringLong.java
+++ Arrays/part-4/substringLong.java
@@ -1,18 +1,40 @@
 import java.util.*;
 
+
 public class substringLong{
     public static int countSubstringLength(String s){
-        int count = 0;
+        // int count = 0;
+        // HashSet<Character> set = new HashSet<>();
+        // for(char c : s.toCharArray()){
+        //     set.add(c);
+        // }
+
+        // for(char c : set){
+        //     count++;
+        // }
+
+        // return count;
+        // int count = 0;
+        // for(int i=0 ; i<s.length() ; i++){
+        //     for(int j=i+1 ; j<s.length() ; j++){
+        //         if(s.charAt(i)==s.charAt(j)) continue;
+        //         else count++;
+        //     }
+        // }
+        // return count;
+
+         int left=0, maxlen = 0;
         HashSet<Character> set = new HashSet<>();
-        for(char c : s.toCharArray()){
-            set.add(c);
-        }
+        for(int i = 0 ;i<s.length() ; i++){
+            while(set.contains(s.charAt(i))){
+                set.remove(s.charAt(i));
+                left++;
+            }
 
-        for(char c : set){
-            count++;
+            set.add(s.charAt(i));
+            maxlen = Math.max(maxlen,i-left+1);
         }
-
-        return count;
+        return maxlen;
         
     }
     public static void main(String args[]){
