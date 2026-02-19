309d97879e8b7ec51dc6fec2b2a213b72eac213a
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-02-19 16:24:24 +0530

    reverse k nodes of linked list

---
+++ LinkedList/Part-2/reverse2.java
@@ -0,0 +1,49 @@
+public class reverse2{
+    static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node reverseK(Node head, int k){
+        if(head == null || k<=1) return head;
+
+        Node temp = head;
+        for(int i=0 ; i<k ; i++){
+            temp = temp.next;
+            if(temp==null) return head;
+        }
+
+        Node curr = head;
+        Node prev = new Node(-1);
+        for(int i=0 ; i<k ; i++){
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head.next = reverseK(curr, k);
+        return prev;
+
+    }
+
+    public static void main(String args[]){
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        int k = 3;
+        Node ans = reverseK(head,k);
+        Node temp = ans;
+        while(temp!=null){
+            System.out.println(temp.data + "->");
+            temp = temp.next;
+        }
+
+    }
+}
