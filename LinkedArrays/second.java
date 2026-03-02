05c8fa90b978599e928d6ab1733222f987c893bf
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-03-02 15:04:12 +0530

    interleaving technique

--- LinkedArrays/second.java
+++ LinkedArrays/second.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class second{
-    static class Node{
+    public static class Node{
         int data;
         Node next;
         Node random;
@@ -31,6 +31,39 @@ public class second{
 
         return map.get(head);
     }
+    public static Node copyRandomList2(Node head){
+        if(head==null) return head;
+        Node curr = head;
+        while(curr!=null){
+            Node copy = new Node(curr.data);
+            Node nextOriginal = curr.next;
+
+            curr.next = copy;
+            copy.next = nextOriginal;
+
+            curr = nextOriginal;
+        }
+        curr = head;
+        while(curr!=null){
+            if(curr.random!=null){
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node clonedHead = head.next;
+        while(curr!=null){
+            Node copy = curr.next;
+            curr.next = copy.next;
+
+            if(copy.next!=null){
+                copy.next = copy.next.next;
+            }
+            curr = curr.next;
+        }
+        return clonedHead;
+    }
 
         public static void main(String[] args) {
             Node head = new Node(7);
@@ -44,7 +77,7 @@ public class second{
             head.next.next.random = head.next.next.next;
             head.next.next.next.random = head.next;
 
-            Node ans = copyRandomList(head);
+            Node ans = copyRandomList2(head);
             Node temp = ans;
             while(temp!=null){
                 System.out.print(temp.data + "'");
