7b929481ffdb98a26af68d99fc46a024ba67277e
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-03-09 16:04:14 +0530

    maximum length of pair chains

---
+++ Greedy/third.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class third{
+    public static int maxPairChain(int[][]pairs){
+        int length = 1;
+        Arrays.sort(pairs, Comparator.comparingInt(a->a[1]));
+        int lastend = pairs[0][1];
+
+        for(int i=1 ; i<pairs.length ; i++){
+            if(pairs[i][1]>lastend){
+                length++;
+                lastend = pairs[i][0];
+            }
+        }
+        return length;
+    }
+    public static void main(String[] args) {
+        int pairs[][] = {{1,2},{7,8}, {4,5}};
+        System.out.println("Length of chain : " + maxPairChain(pairs));
+    }
+}
