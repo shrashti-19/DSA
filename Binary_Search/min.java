e52a0b7f073597dc38271d1c5a7ef84b8abfeb39
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-04-13 15:36:38 +0530

    min element using O(logn) binary search

---
+++ Binary_Search/min.java
@@ -0,0 +1,21 @@
+public class min{
+    public static int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low<high){
+            int mid = low +  (high-low)/2;
+
+            if(nums[mid]>nums[high]){
+                low = mid+1;
+            }else{
+                high = mid;
+            }
+
+        }
+        return nums[low];
+    }
+    public static void main(String args[]){
+        int nums[] = {4,5,6,1,2,3};
+        System.out.println("Min element is : " + findMin(nums));
+    }
+}
